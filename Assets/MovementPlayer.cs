6029bbfb9f22789bb9d82c3db1b42858d83b094e
DaddyGanzux <Garm0607@gmail.com>
2025-09-03 09:01:06 -0600

    🚧

--- Assets/MovementPlayer.cs
+++ Assets/MovementPlayer.cs
@@ -6,19 +6,22 @@ using UnityEngine;
 // Class - Forma de declarar 
 // MovementPlayer - Nombre del script
 // : Herencia - permite usar las funciones y variables de MonoBehavior 
-public class MovementPlayer:MonoBehaviour 
+public class MovementPlayer : MonoBehaviour
 {
 
-    //Variables
+    public Transform transformPlayer;
+    public Rigidbody2D rigidBody2DPlayer;
 
-    //float
-    //int
-    //string
-    //bool
-    //char
+    /*Variables
 
-    public float numero = 15f;
+    float
+    int
+    string
+    bool
+    char*/
+
     private int VidaPersonaje = 0;
+    public float numero = 15f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,18 +30,45 @@ public class MovementPlayer:MonoBehaviour
 
         Debug.Log("Start empieza aqui");
 
+        rigidBody2DPlayer = GetComponent<Rigidbody2D>();   //Jala el rb del objeto 
+
     }//End Start
 
     // Update is called once per frame
     void Update()
     {
 
-        Debug.Log("Update empieza aqui");
+        //Moverse a la izquierda
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transformPlayer.position += new Vector3(-5, 0, 0) * Time.deltaTime;
+            Debug.Log("Vamos a la izquierda");
+
+            //rigidBody2DPlayer
+        }
+
+        //Moverse a la Derecha
+        if (Input.GetKey(KeyCode.D))
+        {
+            transformPlayer.position += new Vector3(5, 0, 0) * Time.deltaTime;
+            Debug.Log("Vamos a la Derecha");
+        }
+
+        //Saltar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transformPlayer.position += new Vector3(0, 500, 0) * Time.deltaTime;
+            Debug.Log("Saltemos");
+        }
+
+
+
+       /* Debug.Log("Update empieza aqui");
 
         if (VidaPersonaje <= 0)
         {
             Debug.Log("Mamaste");
-        }
+        }*/
 
     }//End Update 
 
